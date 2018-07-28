fb24cceeb37dacf284d1d66f5e0237040c4c1a5c
kim <315197141@qq.com>
2018-07-28 15:26:57 +0800

    增加房屋信息接口

--- handlers/House.py
+++ handlers/House.py
@@ -51,7 +51,7 @@ class AreaInfoHAndler(BaseHandler):
         self.write(dict(errcode=RET.OK, errmsg="OK", data=data))
 
 
-""""""
+"""查询房源"""
 
 
 class MyHousesHandler(BaseHandler):
@@ -81,4 +81,74 @@ class MyHousesHandler(BaseHandler):
                 houses.append(house)
         self.write({"errcode": RET.OK, "errmsg": "OK", "houses": houses})
 
-    pass
+
+class HouseInfoHandler(BaseHandler):
+    """房屋信息"""
+
+    @required_login
+    def post(self):
+        """保存"""
+        # 获取参数
+        user_id = self.session.data.get("user_id")
+        title = self.json_args.get("title")
+        price = self.json_args.get("price")
+        area_id = self.json_args.get("area_id")
+        address = self.json_args.get("address")
+        room_count = self.json_args.get("room_count")
+        acreage = self.json_args.get("acreage")
+        unit = self.json_args.get("unit")
+        capacity = self.json_args.get("capacity")
+        beds = self.json_args.get("beds")
+        deposit = self.json_args.get("deposit")
+        min_days = self.json_args.get("min_days")
+        max_days = self.json_args.get("max_days")
+        facility = self.json_args.get("facility")  # 对一个房屋的设施，是列表类型
+        # 校验
+
+        if not all((title.price, area_id, address, room_count, acreage, unit, capacity, beds, deposit, min_days,
+                    max_days)):
+            return self.write(dict(errcode=RET.PARAMERR, errmsg="缺少参数"))
+
+        try:
+            price = int(price) * 100
+            deposit = int(deposit) * 100
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(errcode=RET.PARAMERR, errmsg="参数错误"))
+
+        # 数据
+        try:
+            sql = "insert into ih_house_info(hi_user_id,hi_title,hi_price,hi_area_id,hi_address,hi_room_count," \
+                  "hi_acreage,hi_house_unit,hi_capacity,hi_beds,hi_deposit,hi_min_days,hi_max_days) " \
+                  "values(%(user_id)s,%(title)s,%(price)s,%(area_id)s,%(address)s,%(room_count)s,%(acreage)s," \
+                  "%(house_unit)s,%(capacity)s,%(beds)s,%(deposit)s,%(min_days)s,%(max_days)s)"
+
+            house_id = self.db.execute(sql, user_id=user_id, title=title, price=price, area_id=area_id, address=address,
+                                       room_count=room_count, acreage=acreage, house_unit=unit, capacity=capacity,
+                                       beds=beds, deposit=deposit, min_days=min_days, max_days=max_days)
+        except Exception as e:
+            logging.error(e)
+            return self.write(dict(errcode=RET.DBERR, errmsg="save data error"))
+
+        try:
+            sql = "insert into ih_home_facility(hf_house_id,hf_facility_id) values"
+            sql_val = []
+            vals = []
+            for facility_id in facility:
+                sql_val.append("(%s,%s)")
+                vals.append(house_id)
+                vals.append(facility_id)
+            sql += ",".join(sql_val)
+            vals = tuple(vals)  # 转换为元组
+            self.db.execute(sql, *vals)
+        except Exception as e:
+            logging.error(e)
+            try:
+                self.db.execute("delete from ih_house_info where hi_house_id=%s", house_id)
+            except Exception as e:
+                logging.error(e)
+                return self.write(dict(errcode=RET.DBERR, errmsg="delete fail"))
+            else:
+                return self.write(dict(errcode=RET.DBERR, errmsg="no data save"))
+        # 返回
+        self.write(dict(errcode=RET.OK, errmsg="OK", house_id=house_id))
